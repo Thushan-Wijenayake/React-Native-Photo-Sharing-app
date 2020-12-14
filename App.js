7149be5d7c5268cf03397c7ded27f4852ce5b976
Thushan Wijenayake <60375841+Thushan-Wijenayake@users.noreply.github.com>
2020-12-14 15:22:08 +0530

    Adding an image using URL or local path

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
+
+        {/*<Image source={logo} style={{ width: 305, height: 159 }} />*/}
+
+        {/*Loading image by using URL*/}
+        <Image source={{ uri: "https://i.imgur.com/TkIrScD.png"}} style={{ width: 305, height: 159 }} />
+
+
       <Text style={{color: '#FF0000', fontSize: 18}}>
         Add custom style
       </Text>
