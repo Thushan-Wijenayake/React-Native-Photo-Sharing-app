86e0c9ad98e4f3d78e9a67fb79b2bb1917a29abb
Thushan Wijenayake <60375841+Thushan-Wijenayake@users.noreply.github.com>
2020-12-21 14:58:54 +0530

    Creating a button

--- App.js
+++ App.js
@@ -1,31 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import logo from './assets/logo.png';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
+    return (
+        <View style={styles.container}>
 
-        {/*<Image source={logo} style={{ width: 305, height: 159 }} />*/}
+            {/*Loading image by using URL*/}
+            <Image source={{uri: "https://i.imgur.com/TkIrScD.png"}} style={styles.logo}/>
+            <Text style={styles.instructions}>
+                To share a photo from your phone with a friend, just click a button
+            </Text>
 
-        {/*Loading image by using URL*/}
-        <Image source={{ uri: "https://i.imgur.com/TkIrScD.png"}} style={{ width: 305, height: 159 }} />
+            <TouchableOpacity
+                onPress={() => alert('Hello world')}
+                style={styles.button}>
+                <Text style={styles.buttonText}>Pick a Photo</Text>
+            </TouchableOpacity>
 
-
-      <Text style={{color: '#FF0000', fontSize: 18}}>
-        Add custom style
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        width: 305,
+        height: 159,
+        marginBottom: 20,
+    },
+    instructions: {
+        color: '#888',
+        fontSize: 18,
+        marginHorizontal: 15,
+        marginBottom: 10,
+    },
+    button: {
+        backgroundColor: "blue",
+        padding: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#fff',
+    },
 });
